73875f155f018c57c3e6957be623deaf276e91f1
Lucas Darnell <32601048+fifostack@users.noreply.github.com>
2018-01-24 11:42:42 -0500

    Refined item class and started item interactions

--- src/item/item.java
+++ src/item/item.java
@@ -5,15 +5,33 @@ package item;
 This class will define items
 */
 
+import java.util.LinkedList;
 public class item {
     
     private String itemName;
     private int maxStack; //maximum that can occupy one slot
+    private LinkedList<item> useWith; //what other items this can be used with
     
     public item(String n, int stack)
     {
         itemName = n;
         maxStack = stack;
+	useWith = new LinkedList<item>();
     }
-    
+
+    public void setName(String n) {
+	itemName = n;
+    }
+    public void setInteractions(LinkedList<item> x) {
+	useWith = x;
+    }
+    public String getItemName() {
+	return itemName;
+    }
+    public int getMaxStack() {
+	return maxStack;
+    }
+    public LinkedList<item> getInteractions() {
+	return useWith;
+    }
 }
